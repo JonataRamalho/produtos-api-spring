b310d234f3db471dce7c4d2dc6d252f4de4e6587
jonata.ramalho <jonata.ramalho.ext@ipiranga.ipiranga>
2025-02-10 17:36:44 -0300

    feat: esinicia projeto

---
+++ src/test/java/io/github/jonata/produtosapi/ProdutosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jonata.produtosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
