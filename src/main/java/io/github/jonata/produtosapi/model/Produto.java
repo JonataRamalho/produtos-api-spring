b2adf80d2f1b5556ebc859a69e322a0aa4f357d0
jonata.ramalho <jonata.ramalho.ext@ipiranga.ipiranga>
2025-02-13 18:48:20 -0300

    feat: cria entidade produto

---
+++ src/main/java/io/github/jonata/produtosapi/model/Produto.java
@@ -0,0 +1,66 @@
+package io.github.jonata.produtosapi.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class Produto {
+
+    @Id
+    @Column
+    private String id;
+
+    @Column
+    private String nome;
+
+    @Column
+    private String descricao;
+
+    @Column
+    private String preco;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getPreco() {
+        return preco;
+    }
+
+    public void setPreco(String preco) {
+        this.preco = preco;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "id='" + id + '\'' +
+                ", nome='" + nome + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", preco='" + preco + '\'' +
+                '}';
+    }
+}
